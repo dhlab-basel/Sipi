cc3425f91f5dd22b04e9c43aafdfb335f2bc91db
Ivan Subotic <ibs@subotic.ch>
2017-07-07 10:59:20 +0200

    test (scripts): download test image

--- test/test_02_server.py
+++ test/test_02_server.py
@@ -64,7 +64,8 @@ class TestServer:
 
     def test_lausane_thumbnail(self, manager):
         """create thumbnail with Lausanne file"""
-        response_json = manager.post_file("/make_thumbnail", manager.data_dir_path("knora/ru09_sp_f_rn_12_06_kat-r.tif"), "image/tiff")
-        print(response_json)
+        local_file_path = manager.download_file_to_data_dir_tmp("https://drive.switch.ch/index.php/s/FUa0Q9xbAN4FVDZ/download", ".tif")
+        response_json = manager.post_file("/make_thumbnail", local_file_path, "image/tiff")
+        print("\n==>>" + str(response_json))
         filename = response_json["filename"]
         manager.expect_status_code("/thumbs/{}.jpg/full/full/0/default.jpg".format(filename), 200)
