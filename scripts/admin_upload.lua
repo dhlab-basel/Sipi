c13b8730024fac2719c484f95337913abfd11981
André Kilchenmann <a.kilchenmann@unibas.ch>
2017-12-08 10:45:45 +0100

    enhancement (admin upload): return correct error messages; update copyright information; return proper answer

--- scripts/admin_upload.lua
+++ scripts/admin_upload.lua
@@ -1,9 +1,24 @@
 --
--- Created by IntelliJ IDEA.
--- User: ak
--- Date: 05.12.17
--- Time: 15:27
--- To change this template use File | Settings | File Templates.
+-- Copyright © 2016 Lukas Rosenthaler, Andrea Bianco, Benjamin Geer,
+-- Ivan Subotic, Tobias Schweizer, André Kilchenmann, and André Fatton.
+-- This file is part of Sipi.
+-- Sipi is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as published
+-- by the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+-- Sipi is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+-- Additional permission under GNU AGPL version 3 section 7:
+-- If you modify this Program, or any covered work, by linking or combining
+-- it with Kakadu (or a modified version of that library), containing parts
+-- covered by the terms of the Kakadu Software Licence, the licensors of this
+-- Program grant you additional permission to convey the resulting work.
+-- See the GNU Affero General Public License for more details.
+-- You should have received a copy of the GNU Affero General Public
+-- License along with Sipi.  If not, see <http://www.gnu.org/licenses/>.
+
+-- upload script for project logos (images), user profile avatar, and project specific icons used in salsah admin
 --
 require "send_response"
 
@@ -23,15 +38,15 @@ for findex,fparam in pairs(server.uploads) do
     admindir = config.docroot .. '/admin/'
     local success, exists = server.fs.exists(admindir)
     if not success then
-        server.sendStatus(500)
-        server.log(exists, server.loglevel.error)
+        server.log("server.fs.exists() failed: " .. exists, server.loglevel.LOG_ERR)
+        send_error(500, "Internal server error")
         return false
     end
     if not exists then
         local success, errmsg = server.fs.mkdir(admindir, 511)
         if not success then
-            server.sendStatus(500)
-            server.log(errmsg, server.loglevel.error)
+            server.log("server.fs.mkdir() failed: " .. errmsg, server.loglevel.LOG_ERR)
+            send_error(500, "Admin directory could not be created on server")
             return false
         end
     end
@@ -39,15 +54,14 @@ for findex,fparam in pairs(server.uploads) do
     --
     -- copy the file to admin directory
     --
-
     success, uuid62 = server.uuid62()
     origname = fparam["origname"]:gsub("%s+", "-")
 
     adminpath =  admindir .. uuid62 .. '-' .. origname
     local success, errmsg = server.copyTmpfile(findex, adminpath)
     if not success then
-        server.sendStatus(500)
         server.log(errmsg, server.loglevel.error)
+        send_error(500, "Couldn't upload file: " .. result)
         return false
     else
         files[findex] = uuid62 .. '-' .. origname
@@ -55,7 +69,6 @@ for findex,fparam in pairs(server.uploads) do
 end
 
 answer = {
-    status = "OK",
     files = files
 }
 
