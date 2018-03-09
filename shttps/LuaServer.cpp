11e565bc3f3c8696f48e69cd7360b8c3bcfaea2a
Lukas Rosenthaler <lukas.rosenthaler@unibas.ch>
2018-03-09 16:14:52 +0100

    beautifying

--- shttps/LuaServer.cpp
+++ shttps/LuaServer.cpp
@@ -208,8 +208,9 @@ namespace shttps {
             char_array_4[i++] = encoded_string[in_];
             in_++;
             if (i == 4) {
-                for (i = 0; i < 4; i++)
+                for (i = 0; i < 4; i++) {
                     char_array_4[i] = base64_chars.find(char_array_4[i]);
+                }
 
                 char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
                 char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
