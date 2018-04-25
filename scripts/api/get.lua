2d947af57ac594fa901803a0c517364f078ebc90
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2018-04-25 17:31:13 +0200

    url is searched by pattern, pattern matching tests

--- scripts/api/get.lua
+++ scripts/api/get.lua
@@ -24,78 +24,116 @@ table1 = {}
 element = {}
 elements = {}
 
--- Find ID from the URL
-local id
-startPos, endPos = string.find(server.uri, "api/resources/")
 
-if (startPos ~= nil) and (endPos ~= nil) then
-    local num = tonumber(string.sub(server.uri, endPos+1, string.len(server.uri)))
-    if (num ~= nil) then
-        id = math.floor(num)
+local filePattern = "api/resources/%d+/file$"
+local idPattern = "api/resources/%d+$"
+local resourcePattern = "api/resources$"
+
+function hasFilePattern(url, filePattern)
+    local i, j = string.find(url, filePattern)
+    if (i~=nil) and (j ~= nil) then
+        return string.match(url,"%d+")
+    else
+        return nil
     end
 end
 
--- Gets data with the given ID
-if id ~= nil then
-    local db = sqlite("testDB/testData.db", "RW")
-    local qry = db << selectIDQuery(id)
-    local row = qry()
-
-    if row ~= nil then
-        -- ACHTUNG DIES IST LUA SYNTAKTISCH FALSCH
-        element["id"] = row[0]
-        element["title"] = row[1]
-        element["date"] = row[2]
-        table1["data"]  = element
-        table1["status"] = successStatus()
+function hasIDPattern(url, idPattern)
+    local z = string.match(url, idPattern)
+    if (z~=nil) then
+        return string.match(url, "%d+")
     else
-        table1["data"]  = { element }
-        table1["status"] = noDataStatus()
+        return nil
     end
+end
 
-    qry = ~qry -- delete query and free prepared statment
-    db = ~db -- delete the database connection
+function hasResourcePattern(url, pattern)
+    local z = string.match(url, pattern)
+    if (z~=nil) then
+        return true
+    else
+        return false
+    end
+end
 
--- Gets all the data
+local uri = server.uri
+
+local id = hasFilePattern(uri, filePattern)
+if (id ~= nil) then
+    print(uri .. " ==> has FilePattern with = " .. id)
 else
+    id = hasIDPattern(uri, idPattern)
+    if (id ~= nil) then
+        print(uri .. " ==> has IDPattern with = " .. id)
 
-    -- Inserts all the parameters
-    local parameters = {}
-    if (server.get ~= nil) then
-        for key,value in pairs(server.get) do
-            -- Equal search
-            -- table.insert(parameters, key ..'=' .. '"' .. value .. '"')
-            -- Like search
-            table.insert(parameters, key ..' like ' .. '"%' .. value .. '%"')
+        local db = sqlite("testDB/testData.db", "RW")
+        local qry = db << selectIDQuery(id)
+        local row = qry()
+
+        if row ~= nil then
+            -- ACHTUNG DIES IST LUA SYNTAKTISCH FALSCH
+            element["id"] = row[0]
+            element["title"] = row[1]
+            element["date"] = row[2]
+            table1["data"]  = element
+            table1["status"] = successStatus()
+        else
+            table1["data"]  = { element }
+            table1["status"] = noDataStatus()
+        end
+
+        qry = ~qry -- delete query and free prepared statment
+        db = ~db -- delete the database connection
+
+    else
+        if (hasResourcePattern(uri, resourcePattern)) then
+            print(uri .. " ==> has ressourcePattern")
+
+            -- Inserts all the parameters
+            local parameters = {}
+            if (server.get ~= nil) then
+                for key,value in pairs(server.get) do
+                    -- Equal search
+                    -- table.insert(parameters, key ..'=' .. '"' .. value .. '"')
+                    -- Like search
+                    table.insert(parameters, key ..' like ' .. '"%' .. value .. '%"')
+                end
+            end
+
+            local db = sqlite("testDB/testData.db", "RW")
+            local qry
+
+            if (#parameters ~= 0) then
+                qry = db << selectParametersQuery(table.concat(parameters, " AND "))
+            else
+                qry = db << selectAllQuery()
+            end
+
+            local row = qry()
+
+            while (row) do
+                table.insert(elements, {["id"] = row[0],["title"]= row[1],["date"]=row[2]})
+                row = qry()
+            end
+
+            table1["data"] =  elements
+
+            if #elements > 0 then
+                table1["status"] = successStatus()
+            else
+                table1["status"] = noDataStatus()
+            end
+
+            qry = ~qry -- delete query and free prepared statment
+            db = ~db -- delete the database connection
+
+        else
+            print(uri .. " ==> FAIL")
+
+            server.sendStatus(404)
+            return
         end
     end
-
-    local db = sqlite("testDB/testData.db", "RW")
-    local qry
-
-    if (#parameters ~= 0) then
-        qry = db << selectParametersQuery(table.concat(parameters, " AND "))
-    else
-        qry = db << selectAllQuery()
-    end
-
-    local row = qry()
-
-    while (row) do
-        table.insert(elements, {["id"] = row[0],["title"]= row[1],["date"]=row[2]})
-        row = qry()
-    end
-
-    table1["data"] =  elements
-
-    if #elements > 0 then
-        table1["status"] = successStatus()
-    else
-        table1["status"] = noDataStatus()
-    end
-
-    qry = ~qry -- delete query and free prepared statment
-    db = ~db -- delete the database connection
 end
 
 
--- scripts/api/playground.lua
+++ scripts/api/playground.lua
@@ -59,22 +59,101 @@ page = [[
     </html>
 ]]
 
-a = {}
-for i = 1, 1000
-    do a[i] = i*2
---    print(a[i])
-end
+--a = {}
+--for i = 1, 1000
+--    do a[i] = i*2
+----    print(a[i])
+--end
+--
+--s = "api/resources/"
+--
+--i, j = string.find(s, "api/resources/")
+--local id
+--if (i ~= nil) and (j ~= nil) then
+--    i = tonumber(string.sub(s, j+1, string.len(s)))
+--    if (i ~= nil) and (type(i)) == "number" then
+--        id = i
+--    end
+--end
 
-s = "api/resources/"
+regexTable = {}
+regexTable[1] = "api/resources"
+regexTable[2] = "api/resources/"
+regexTable[3] = "api/resources/0"
+regexTable[4] = "api/resources/1"
+regexTable[5] = "api/resources/00"
+regexTable[6] = "api/resources/2"
+regexTable[7] = "api/resources/183"
+regexTable[8] = "api/resources/0.3"
+regexTable[9] = "api/resources/1.5"
+regexTable[10] = "api/resources//"
+regexTable[11] = "api/resources/a"
+regexTable[12] = "api/resources/a3"
+regexTable[13] = "api/resources/67/"
+regexTable[14] = "api/resources/13/file"
+regexTable[15] = "api/resources/13/file/"
+regexTable[16] = "api/resources/13/file"
+regexTable[17] = "api/resources/1.3/file"
+regexTable[18] = "api/resources/13/file/34"
+regexTable[19] = "api/resources/0/file"
+regexTable[20] = "api/resources/-0/file"
+regexTable[21] = "api/resources/012345630/file"
+regexTable[22] = "api/resources/2.0030/file"
+regexTable[23] = "api/resources/2,345/file"
+regexTable[24] = "api/resources/-0.3/file"
+regexTable[25] = "api/resources/-0,3/file"
 
-i, j = string.find(s, "api/resources/")
-local id
-if (i ~= nil) and (j ~= nil) then
-    i = tonumber(string.sub(s, j+1, string.len(s)))
-    if (i ~= nil) and (type(i)) == "number" then
-        id = i
+local filePattern = "api/resources/%d+/file$"
+local idPattern = "api/resources/%d+$"
+local resourcePattern = "api/resources$"
+
+function hasFilePattern(url, filePattern)
+    local i, j = string.find(url, filePattern)
+    if (i~=nil) and (j ~= nil) then
+        return string.match(url,"%d+")
+    else
+        return nil
     end
 end
+
+function hasIDPattern(url, idPattern)
+    local z = string.match(url, idPattern)
+    if (z~=nil) then
+        return string.match(url, "%d+")
+    else
+        return nil
+    end
+end
+
+function hasResourcePattern(url, pattern)
+    local z = string.match(url, pattern)
+    if z~=nil then
+        return true
+    else
+        return false
+    end
+end
+
+
+local id
+for k,v in pairs(regexTable) do
+    local id = hasFilePattern(v, filePattern)
+    if (id ~= nil) then
+        print(v .. "=> has FilePattern with = " .. id)
+    else
+        id = hasIDPattern(v, idPattern)
+        if (id ~= nil) then
+            print(v .. "=> has IDPattern with = " .. id)
+        else
+            if (hasResourcePattern(v, resourcePattern)) then
+                print(v .. "=> has ressourcePattern")
+            else
+                print(v .. "=> FAIL")
+            end
+        end
+    end
+end
+
 
 Monster = {}
 Monster.__index = Monster
@@ -92,10 +171,10 @@ function Monster:WarCry()
     print(self.name .. ": Graahh!!!!")
 end
 
-monster_01 = Monster:Create()
-monster_01: WarCry()
-
-print(monster_01)
+--monster_01 = Monster:Create()
+--monster_01: WarCry()
+--
+--print(monster_01)
 
 local steuersatz = {
     zehn = 1.2,
@@ -118,7 +197,7 @@ steuersatz.steuerberechnung =  function(satz, geldmenge)
     end
 end
 
-print(steuersatz:steuerberechnung(10))
+--print(steuersatz:steuerberechnung(10))
 
 meta =
 {
@@ -129,8 +208,8 @@ meta =
 
 doubleTalbe = {}
 setmetatable(doubleTalbe, meta)
-print(doubleTalbe[2])
-print(doubleTalbe[16])
+--print(doubleTalbe[2])
+--print(doubleTalbe[16])
 
 parent = { name = "default name", title = "Mr" }
 meta = { __index = parent }
@@ -138,20 +217,10 @@ child = { name = "Bob" }
 
 setmetatable(child, meta)
 
-print(child.title, child.name)
+--print(child.title, child.name)
 child.name = nil
-print(child.title, child.name)
+--print(child.title, child.name)
 
-
---local id
---pattern = "%d+"
---i, j = string.find(s, pattern)
---if (i ~= nil) and (j ~= nil) then
---    a = string.sub(s,i, j)
---    if (a ~= nil) and (type(i)) == "number" then
---        id = a
---    end
---end
 
 -- File reading
 --io.input("vogel.jpg")
