f4134f0f7bfa0da6d28c91b3b3183a61f14b2640
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2018-09-02 15:45:38 +0200

    first version of login

--- scripts/api/login/post.lua
+++ scripts/api/login/post.lua
@@ -1,2 +1,21 @@
 print("---- POST login script ----")
 
+local name = "admin"
+local pw = "1234"
+
+local sendName, sendPW
+for key, value in pairs(server.post) do
+    if (key == "name") then
+        sendName = value
+    elseif (key == "password") then
+        sendPW = value
+    end
+end
+
+if (sendName == name) and (sendPW == pw) then
+    server.sendHeader('Content-type', 'application/json')
+    server.sendStatus(200)
+else
+    server.sendHeader('Content-type', 'application/json')
+    server.sendStatus(401)
+end
