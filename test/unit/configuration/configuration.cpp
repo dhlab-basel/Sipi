8c31ee1b7eb1de7e0fa29cb1e772158b1b76155e
Ivan Subotic <400790+subotic@users.noreply.github.com>
2020-08-14 17:59:06 +0200

    reset config to default values

--- test/unit/configuration/configuration.cpp
+++ test/unit/configuration/configuration.cpp
@@ -54,11 +54,11 @@ TEST(Configuration, LoadConfigurationFile)
     EXPECT_EQ(sipiConf.getSubdirExcludes().size(), 2);
     EXPECT_EQ(sipiConf.getInitScript(), "./config/sipi.init.lua");
     EXPECT_EQ(sipiConf.getCacheDir(), "./cache");
-    EXPECT_EQ(sipiConf.getCacheSize(), 20  * 1024 * 1024);
-    EXPECT_EQ(sipiConf.getCacheNFiles(), 8);
+    EXPECT_EQ(sipiConf.getCacheSize(), 200  * 1024 * 1024);
+    EXPECT_EQ(sipiConf.getCacheNFiles(), 250);
     EXPECT_EQ(sipiConf.getCacheHysteresis(), (float) 0.15);
     EXPECT_EQ(sipiConf.getScriptDir(), "./scripts");
     EXPECT_EQ(sipiConf.getThumbSize(), "!128,128");
     EXPECT_EQ(sipiConf.getTmpDir(), "/tmp");
-    EXPECT_EQ(sipiConf.getLoglevel(), "DEBUG");
+    EXPECT_EQ(sipiConf.getLoglevel(), "ERROR");
 }
