5a83ef92edf3e53d9911e82a9082f02c02d9d392
Ralf Barkow <ralf.barkow@dasch.swiss>
2020-09-24 07:27:21 +0200

    fix init DSP-687 SIPI version strings hardcoded somewhere

---
+++ include/SipiConfig.h
@@ -0,0 +1,12 @@
+#ifndef __defined_config_h
+#define __defined_config_h
+
+
+#define HAVE_MALLOC_H 1
+/* #undef HAVE_ST_ATIMESPEC */
+
+/* FIXME DSP-687 SIPI version strings hardcoded */
+#define SIPI_BUILD_DATE "Sipi Version v2.0.0-SNAPSHOT (build 2020-09-16 13:06)"
+#define SIPI_BUILD_VERSION "Sipi Source Git-Version: 30a105d"
+
+#endif
