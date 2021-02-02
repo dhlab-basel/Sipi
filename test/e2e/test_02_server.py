4a23b9d3768ccde60815db3fdff68fb1884b35bf
Lukas Rosenthaler <lukas.rosenthaler@unibas.ch>
2021-02-02 14:44:30 +0100

    add: sidecar support (#344)

--- test/e2e/test_02_server.py
+++ test/e2e/test_02_server.py
@@ -240,7 +240,10 @@ class TestServer:
                 "expected_result": {
                     "@context": "http://sipi.io/api/file/3/context.json",
                     "id": "http://127.0.0.1:1024/unit/",
+                    "originalFilename": "test.csv",
                     "internalMimeType": "text/csv",
+                    "checksumDerivative": "228beaf29a9cdc864f777107c8f1a7bf9243d4140a844b7117a81d7d91fa6e37",
+                    "checksumOriginal": "228beaf29a9cdc864f777107c8f1a7bf9243d4140a844b7117a81d7d91fa6e37",
                     "fileSize": 39697
                 }
             }
